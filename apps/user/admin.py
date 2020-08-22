eebbf813c9f50aaed988b1df6cf56932d9ad48fa
zhangqi19900214d <zhangqi_gsts@foxmail.com>
2020-08-22 16:52:39 +0800

    显示用户足迹和验证码

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.auth.models import User
 
-from apps.user.models import UserProfile
+from apps.user.models import UserProfile, VisitHistory, SMSStatus
 
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -24,6 +24,16 @@ class ProfileAdmin(admin.ModelAdmin):
     header.short_description = '头像'
 
 
+class SMSStatusAdmin(admin.ModelAdmin):
+    list_display = ["id", "phone", "code", "create_time"]
+
+
+class VisitHistoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "ip_str", "url", "visit_time"]
+
+
 admin.site.unregister(User)
 admin.site.register(User, ProfileAdmin)
-admin.site.register(UserProfile, ProfileAdmin)
+# admin.site.register(UserProfile, ProfileAdmin)
+admin.site.register(SMSStatus, SMSStatusAdmin)
+admin.site.register(VisitHistory, VisitHistoryAdmin)
